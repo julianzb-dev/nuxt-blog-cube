35fcaa2283989f667d276ef32b688cde9539cfd2
Julian Zapata <julianandreszb@gmail.com>
2025-05-14 01:31:45 -0500

    feat: base skeleton using CUBE CSS

---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,134 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const DARK_MODE_LOCAL_STORAGE_KEY = 'dark-mode'
+let colorSchemeMedia = null
+const isDarkModeEnabled = ref(false)
+
+function getInitialDarkModeState() {
+  const storedPreference = localStorage.getItem(DARK_MODE_LOCAL_STORAGE_KEY)
+  return storedPreference === 'true' || (storedPreference === null && colorSchemeMedia.matches);
+}
+
+function setInitialPreference() {
+  if (localStorage.getItem(DARK_MODE_LOCAL_STORAGE_KEY) === null) {
+    localStorage.setItem(DARK_MODE_LOCAL_STORAGE_KEY, isDarkModeEnabled.value)
+  }
+}
+
+function toggleColorScheme(state) {
+  document.documentElement.classList.toggle('dark', state)
+  localStorage.setItem(DARK_MODE_LOCAL_STORAGE_KEY, state)
+}
+
+function onChangeColorScheme(event) {
+  toggleColorScheme(event.matches)
+}
+
+onMounted(() => {
+  // Initialize browser-only variables here
+  colorSchemeMedia = window.matchMedia('(prefers-color-scheme: dark)')
+  isDarkModeEnabled.value = getInitialDarkModeState()
+
+  setInitialPreference()
+  toggleColorScheme(isDarkModeEnabled.value)
+  colorSchemeMedia.addEventListener('change', onChangeColorScheme)
+})
+
+onUnmounted(() => {
+  if (colorSchemeMedia) {
+    colorSchemeMedia.removeEventListener('change', onChangeColorScheme)
+  }
+})
+</script>
+
+<template>
+  <div class="dark-mode-toggle">
+    <label class="switch" @click="toggleColorScheme(!isDarkModeEnabled)">
+      <input v-model="isDarkModeEnabled" type="checkbox" title="Toggle Dark mode" >
+      <span class="slider">
+        <svg
+          class="slider-icon"
+          viewBox="0 0 24 24"
+          fill="none"
+          height="20"
+          stroke="#000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          width="20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+        </svg>
+      </span>
+    </label>
+  </div>
+</template>
+
+<style scoped>
+
+.dark-mode-toggle {
+  background-color: var(--color-bg-primary);
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: lightblue;
+  border-radius: 34px;
+  transition: 0.6s;
+}
+
+.slider:before {
+  position: absolute;
+  content: '';
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  border-radius: 50%;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #363f72;
+}
+
+input:checked + .slider:before {
+  transform: translateX(26px);
+}
+
+.slider-icon {
+  background-color: transparent;
+  transform: rotate(250deg);
+  position: absolute;
+  right: 6px;
+  top: 7px;
+}
+
+@media (max-width: 768px) {
+  .switch{
+    margin-block: 0.6rem;  
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import logo from 'assets/images/accelerate.svg'
+import ThemeSwitcher from "~/components/ThemeSwitcher.vue";
+</script>
+
+<template>
+  <div>
+    <header>
+      <nav class="navbar | cluster">
+        <a href="/">
+          <img :src="logo" alt="Company Name Logo">
+        </a>
+        <ul class="cluster">
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Blog</a></li>
+          <li>
+            <theme-switcher/>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <section class="hero | stack center">
+          <img class="avatar" src="https://picsum.photos/300" alt="Digital innovation concept illustration">
+          <h2>Julian Zapata</h2>
+          <h1>Full-Stack Mastered</h1>
+          <p>
+            Hey there, welcome! I'm a web developer with 12 years of experience crafting online solutions. I'm
+            particularly passionate about Vue.js – you'll find plenty of insights and discoveries on my blog.
+          </p>
+          <p>
+            Currently, I'm diving into Web Design, Nuxt, and Rust. Please check out my portfolio(coming soon), have a look
+            around, and feel free to reach me out!
+          </p>
+          <div>
+            <nuxt-link class="btn" data-size="xl" data-priority="primary">Contact Me</nuxt-link>
+          </div>
+      </section>
+  </div>
+</template>
+
+<style scoped>
+/* Navbar */
+.navbar {
+  justify-content: space-between;
+}
+
+.navbar img {
+  height: 2rem;
+}
+
+/* Hero section */
+.hero {
+  text-align: center;
+}
+
+.hero > .avatar {
+  width: 12rem;
+  height: 12rem;
+  border-radius: var(--radius-full);
+}
+
+</style>
